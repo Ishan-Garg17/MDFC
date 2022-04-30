7368193ab4c13bb73bf5dbae671dbd9798015980
Ishan-Garg17 <ishangarg17@gmail.com>
2022-04-30 15:45:39 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+import './Navbar.scss'
+import images from '../../assets/images'
+import {HiMenuAlt4, HiX} from 'react-icons/hi'
+
+
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+   <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+          <img src={images.logo} alt="Ishan Garg" />
+        </div>
+        <ul className='app__navbar-links'>
+          {['home','about','work','skills','contact'].map((item)=>(
+              <li className='app__flex p-text' key={`link-${item}`}>
+                <div/>
+                <a href={`#${item}`}>
+                  {item}
+                </a>
+              </li>
+          ))}
+        </ul>
+
+        <div className='app__navbar-menu'>
+            <HiMenuAlt4 onClick={()=> setToggle(true)} />
+
+
+            {toggle && (
+
+              <motion.div>
+
+                
+              </motion.div>
+
+
+            )}
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
